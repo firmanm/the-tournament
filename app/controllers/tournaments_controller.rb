2cb5bb8c0b40fd46f3e4db2993f794523dcd2ec0
tomomichi <tomomichi.onishi@gmail.com>
2017-04-16 23:53:00 +0900

    wip

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -93,31 +93,37 @@ class TournamentsController < ApplicationController
 
 
   def update
-    p params
+    # Players一覧更新時
+    if tournament_params[:teams].present?
+      # success_url = tournament_edit_games_path(@tournament)
+      success_url = tournament_edit_players_path(@tournament)
+      success_notice = I18n.t('flash.players.update.success')
+      failure_url = {action: 'players/edit'}
+      failure_notice = I18n.t('flash.players.update.failure')
+
+      teams = []
+      tournament_params[:teams].lines.each_with_index do |line, i|
+        team = line.chomp.split(",")
+        team_data = (team[0].present?) ? {name: team[0], flag: team[1]} : nil
+        teams << [] if i%2==0
+        teams.last << team_data
+      end
+      p teams.to_json
+      p tournament_params[:teams]
+      tournament_params[:teams] = teams.to_json
+      p tournament_params[:teams]
+    # 基本情報更新
+    else
+      success_url = tournament_edit_players_path(@tournament)
+      success_notice = I18n.t('flash.tournament.update.success')
+      failure_url = {action: 'edit'}
+      failure_notice = I18n.t('flash.tournament.update.failure')
+    end
+    p tournament_params[:teams]
+
+    # p tournament_params
     redirect_to success_url, notice: success_notice
-    # # Players一覧更新時
-    # if params[:tournament][:players_attributes].present?
-    #   success_url = tournament_edit_games_path(@tournament)
-    #   success_notice = I18n.t('flash.players.update.success')
-    #   failure_url = 'players/edit_all'
-    #   failure_notice = I18n.t('flash.players.update.failure')
-    #
-    #   # 一括登録利用時
-    #   players = params[:tournament][:players_all][:players]
-    #   if players != @tournament.players_list
-    #     players.lines.each_with_index do |line, i|
-    #       break if i >= @tournament.size
-    #       params[:tournament][:players_attributes]["#{i}"]["name"] = line.chomp
-    #     end
-    #   end
-    #   params[:tournament][:players_all] = nil
-    # else
-    #   success_url = tournament_edit_players_path(@tournament)
-    #   success_notice = I18n.t('flash.tournament.update.success')
-    #   failure_url = {action: 'edit'}
-    #   failure_notice = I18n.t('flash.tournament.update.failure')
-    # end
-    #
+
     # if @tournament.update(tournament_params)
     #   redirect_to success_url, notice: success_notice
     # else
@@ -158,6 +164,6 @@ class TournamentsController < ApplicationController
     end
 
     def tournament_params
-      params.require(:tournament).permit(:id, :title, :user_id, :detail, :type, :place, :url, :size, :consolation_round, :tag_list, :double_elimination, :scoreless, :facebook_album_id, players_attributes: [:id, :name, :group, :country], players_all: [:players])
+      params.require(:tournament).permit(:id, :title, :user_id, :detail, :type, :place, :url, :size, :consolation_round, :tag_list, :double_elimination, :scoreless, :facebook_album_id, :teams, :results, players_attributes: [:id, :name, :group, :country], players_all: [:players])
     end
 end
