34fc5ef4d07cdcc121fd85015db419a38d413902
tomomichi <tomomichi.onishi@gmail.com>
2017-05-20 00:25:24 +0900

    faqの仕組み実装

---
+++ app/controllers/docs_controller.rb
@@ -0,0 +1,16 @@
+class DocsController < ApplicationController
+  skip_before_action :authenticate_user!
+
+  def index
+    path = "#{Rails.root}/app/views/docs"
+    files = Dir.glob("#{File.expand_path(path)}/**/_*").map{|m| m.split(/app\/views\/docs\//).last}
+    @docs_by_categories = files.group_by{|file| file.split('/')[0] }
+  end
+
+  def show
+    path = "#{Rails.root}/app/views/docs/**/_#{params[:title]}.html.haml"
+    file = Dir.glob("#{File.expand_path(path)}").map{|m| m.split(/app\/views\/docs\//).last}.first
+
+    @category = file.split('/').first
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -36,6 +36,9 @@ Rails.application.routes.draw do
       match '/:id/(:title)', to: 'tournaments#show', via: :get, as: :pretty_tournament, constraints: {title: /[^\/]+/}
     end
 
+    match '/docs', to: 'docs#index', via: :get, as: :docs
+    match '/docs/:title', to: 'docs#show', via: :get, as: :doc, constraints: {title: /[^\/]+/}
+
     match ':action', controller: :static_pages, via: :get
   end
 end
