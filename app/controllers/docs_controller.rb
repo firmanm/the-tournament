82048581cbbde91d7c6ea80eaa406534c4357a72
tomomichi <tomomichi.onishi@gmail.com>
2017-05-21 23:25:38 +0900

    前後の記事へのリンクをつける

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -12,5 +12,12 @@ class DocsController < ApplicationController
     file = Dir.glob("#{File.expand_path(path)}").map{|m| m.split(/app\/views\/docs\//).last}.first
 
     @category = file.split('/').first
+
+    # 前の記事/次の記事を表示
+    category_path = "#{Rails.root}/app/views/docs/#{@category}/_*"
+    category_files = Dir.glob("#{File.expand_path(category_path)}").map{|m| m.split(/app\/views\/docs\//).last}
+    index = category_files.index(file)
+    @prev = category_files[index - 1].split('/').last.delete('_.htmlhaml') if index > 0
+    @next = category_files[index + 1].split('/').last.delete('_.htmlhaml') if index < category_files.length - 1
   end
 end
