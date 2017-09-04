551efa38b0617f73c951ec02ddf0a1acb810be69
tomomichi <tomomichi.onishi@gmail.com>
2017-09-04 23:28:39 +0900

    enrich match comment

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -22,4 +22,15 @@ module ApplicationHelper
     when 4;  380
     end
   end
+
+  def text_url_to_link text
+    URI.extract(text, ['http', 'https']).uniq.each do |url|
+      sub_text = ""
+      sub_text << "<a href=" << url << " target=\"_blank\">" << url << "</a>"
+
+      text.gsub!(url, sub_text)
+    end
+
+    return text
+  end
 end
