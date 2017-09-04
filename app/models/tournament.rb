247971f2e90db785d35e54908edf2080caadee7f
tomomichi <tomomichi.onishi@gmail.com>
2017-09-05 00:41:43 +0900

    fix helper bug

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -326,4 +326,14 @@ class Tournament < ApplicationRecord
 
     self.tag_list.add(tag)
   end
+
+  def text_url_to_link(text)
+    URI.extract(text, ['http', 'https']).uniq.each do |url|
+      sub_text = ""
+      sub_text << "<a href=" << url << " target=\"_blank\">" << url << "</a>"
+
+      text.gsub!(url, sub_text)
+    end
+    text
+  end
 end
